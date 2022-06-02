17ef9e79cc626069f1a1f7b5446cf81ad5de0ff9
Derek Chang <derekchangc00@gmail.com>
2022-06-02 16:21:03 +0700

    Initial Commit

---
+++ Flix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flix
+//
+//  Created by Derek Chang on 6/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flix/FlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlixApp.swift
+//  Flix
+//
+//  Created by Derek Chang on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
